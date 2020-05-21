ebfaac4db1c67552239e3ce02cfd7475fa479e53
Ivan Ignatev <ignatev.is@phystech.edu>
2020-05-22 01:01:03 +0300

    Add hw2 from sem2

--- android-project/app/src/main/java/com/github/ivigns/abbyy/android/MainActivity.kt
+++ android-project/app/src/main/java/com/github/ivigns/abbyy/android/MainActivity.kt
@@ -2,24 +2,38 @@ package com.github.ivigns.abbyy.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity(), MainFragment.NoteListener {
-
-    var noteId: Long? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.setTitle(R.string.caption_main)
 
-        if (supportFragmentManager.findFragmentByTag(NoteFragment.TAG) == null) {
+        val noteFragment = supportFragmentManager.findFragmentByTag(NoteFragment.TAG)
+        val twoFragmentsOnScreen = !(resources.getBoolean(R.bool.isPhone) ||
+                resources.getBoolean(R.bool.isPortrait))
+
+        if ((noteFragment == null) || twoFragmentsOnScreen) {
             val fragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.mainContainer, MainFragment())
+
+            val fragment = if (twoFragmentsOnScreen) R.id.mainListContainer else R.id.mainContainer
+            fragmentTransaction.replace(fragment, MainFragment())
+
             fragmentTransaction.commit()
+        }
+        if (noteFragment != null) {
+            noteFragment.arguments?.getLong(NoteFragment.NOTE_ID)?.let {
+                onNoteClick(it)
+            }
         }
     }
 
     override fun onNoteClick(id: Long) {
+        supportFragmentManager.popBackStackImmediate(NoteFragment.NOTE_ID,
+            FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
         val fragmentTransaction = supportFragmentManager.beginTransaction()
 
         fragmentTransaction.addToBackStack(NoteFragment.NOTE_ID)
@@ -27,8 +41,11 @@ class MainActivity : AppCompatActivity(), MainFragment.NoteListener {
         val args = Bundle()
         args.putLong(NoteFragment.NOTE_ID, id)
         fragment.arguments = args
-        noteId = id
-        fragmentTransaction.replace(R.id.mainContainer, fragment, NoteFragment.TAG)
+        val container = if (resources.getBoolean(R.bool.isPhone) or resources.getBoolean(R.bool.isPortrait))
+            R.id.mainContainer
+        else
+            R.id.mainDetailContainer
+        fragmentTransaction.replace(container, fragment, NoteFragment.TAG)
 
         fragmentTransaction.commit()
     }
--- android-project/app/src/main/java/com/github/ivigns/abbyy/android/MainFragment.kt
+++ android-project/app/src/main/java/com/github/ivigns/abbyy/android/MainFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainFragment : Fragment() {
@@ -25,7 +24,6 @@ class MainFragment : Fragment() {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.setHasFixedSize(true)
         recyclerView.adapter = NoteAdapter(NotesRepository.listNotes(), activity as NoteListener)
-        recyclerView.layoutManager = LinearLayoutManager(view.context)
         return view
     }
 }
